4da8b9fec4d063116710afef552dce793550f480
Albertico <chopytrollface@gmail.com>
2026-09-01 09:54:11 -0600

    Menu terminado 09/01/26 #1

---
+++ Ejercicio0-2/src/main/java/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Methods mt = new Methods();
+        String input;
+
+        while(true){
+            System.out.print("\nInput: ");
+            input = sc.next();
+
+            switch (input) {
+                case "fact" -> {
+                    System.out.print("\nFactorial de: ");
+                    input = sc.next();
+                    System.out.println(" es igual a: "+ mt.factorialRecursivo(Integer.parseInt(input)));
+                }
+
+                case "fibo" -> {
+                    System.out.print("\nFibonacci hasta: ");
+                    input = sc.next();
+                    System.out.print(" es igual a: "+ mt.fibonacciRecursivo(Integer.parseInt(input)));
+                }
+                case "divi" -> {
+                    int divisor, dividendo;
+                    System.out.print("\nDivision de: ");
+                    input = sc.next();
+                    dividendo = Integer.parseInt(input);
+                    System.out.print(" entre ");
+                    input = sc.next();
+                    divisor = Integer.parseInt(input);
+                    System.out.print("es igual a:" + mt.divisionRestasRecursivo(divisor, dividendo));
+                }
+                case "q" -> {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio0-2/src/main/java/Methods.java
@@ -0,0 +1,21 @@
+public class Methods {
+
+    public int factorialRecursivo(int n) {
+        int fac = 0;
+
+        return fac;
+    }
+
+    public int fibonacciRecursivo(int n) {
+        int fib = 0;
+
+        return fib;
+    }
+
+    public int divisionRestasRecursivo(int divisor, int dividendo) {
+        int resultado = 0;
+
+        return resultado;
+    }
+
+}
